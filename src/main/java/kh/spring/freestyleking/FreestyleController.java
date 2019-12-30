22ad8cfd8b098cead48ec66c70f9338728dd9b31
kks <kks@good.com>
2019-12-30 16:22:01 +0900

    hi

---
+++ src/main/java/kh/spring/freestyleking/FreestyleController.java
@@ -0,0 +1,13 @@
+package kh.spring.freestyleking;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FreestyleController {
+ 
+	@RequestMapping(value = "/freestyle")
+	public String hiphop() {
+		return "home";
+	}
+}
